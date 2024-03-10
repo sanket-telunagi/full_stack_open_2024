c1b460af522a5ae5fc35a0d200b32f569759d3a0
sanket-telunagi <telunagisanket@gmail.com>
2024-03-10 23:48:44 +0530

    unicafe complete

--- Part_01/exercises/unicafe/src/App.jsx
+++ Part_01/exercises/unicafe/src/App.jsx
@@ -6,8 +6,18 @@ const Button = ({name, onSmash}) =>{
 }
 
 const StatisticLine = ({text, value}) => {
-  if (text == "positive") return <li>{text} {value}%</li>
-  return <li>{text} {value}</li>
+  if (text == "positive") return (
+    <tr>
+      <td>{text}</td>
+      <td>{value} %</td>
+    </tr>
+  )
+  return (
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
+  )
 }
 
 const Statistics = ({good, neutral, bad}) => {
@@ -20,14 +30,16 @@ const Statistics = ({good, neutral, bad}) => {
   return (
     <>
       <h2>Statistics</h2>
-      <ul>
-        <StatisticLine text="good" value={good}/>
-        <StatisticLine text="neutral" value={neutral}/>
-        <StatisticLine text="bad" value={bad}/>
-        <StatisticLine text="all" value={all}/>
-        <StatisticLine text="average" value={average}/>
-        <StatisticLine text="positive" value={positive}/>
-      </ul>
+      <table>
+        <tbody>
+          <StatisticLine text="good" value={good}/>
+          <StatisticLine text="neutral" value={neutral}/>
+          <StatisticLine text="bad" value={bad}/>
+          <StatisticLine text="all" value={all}/>
+          <StatisticLine text="average" value={average}/>
+          <StatisticLine text="positive" value={positive}/>
+        </tbody>
+      </table>  
     </>
   )
 }
