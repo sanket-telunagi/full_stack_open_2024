2c06d2ce0ca632db177c43a0cfda07dfc48b74c2
Sanket Telunagi <telunagisanket@gmail.com>
2024-06-02 16:29:13 +0530

    Part_03 exercises 3.9 3.10 3.11 deploymet under render test

--- Part_03/exercises/phonebook_backend.js
+++ Part_03/exercises/phonebook_backend.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 const app = express();
+
+app.use(cors());
 
 app.use(
   morgan(function (tokens, req, res) {
@@ -144,7 +147,7 @@ app.delete("/api/persons/:id", (req, res) => {
   res.status(204).end();
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
